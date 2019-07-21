0bfd01a44ca70645534c4170aa1b943ca16c8ca4
tfradebe <tfradebe@gmail.com>
2019-07-21 12:03:19 +0200

    initial commit

---
+++ src/app/item/item.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit {
+
+  @Input() character;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAsign(side){
+    this.character.side = side;
+  }
+
+}
---
+++ src/app/tabs/tabs.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.css']
+})
+export class TabsComponent implements OnInit {
+
+  characters = [
+    { name: 'Luke Skywalker', side: ''},
+    { name: 'Darth Vader', side: ''}
+  ];
+
+  chosenList = "all";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getCharacters(){
+    if(this.chosenList === 'all'){
+      return this.characters.slice();
+    }
+    return this.characters.filter((char)=>{
+      return char.side === this.chosenList;
+    });
+  }
+
+  onChoose(side){
+    this.chosenList = side;
+  }
+
+}
